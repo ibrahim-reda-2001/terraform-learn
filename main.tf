b5b44d8a11f2f673983bc88c796be5c05ed44106
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2024-10-25 16:43:29 +0300

    intial coomit

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA5CBGTGSPWMQHAX2Y"
-  secret_key = "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"
+  
 }
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
