af69fddcaae4e75160a46ff41bd70141b276a937
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2024-10-25 19:55:17 +0300

    first

--- main.tf
+++ main.tf
@@ -1,12 +1,55 @@
 
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   
 }
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
+variable "vpc_cider_block" {
+    type = string
+}
+variable "subnet_cider_block" {
+  type=string
+}
+variable "avail_zone" {
+type = string
+}
+variable "env_perfix" {
+  type = string
+}
+resource "aws_vpc" "myvpc-vpc" {
+  cidr_block = var.vpc_cider_block
   tags = {
-    Name = "main"
+    Name = "${var.env_perfix}-vpc"
   }
 
+}
+  
+
+resource"aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myvpc-vpc.id
+  cidr_block = var.subnet_cider_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name="${var.env_perfix}-subnet"
+  }
+} 
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myvpc-vpc.id
+  tags = {
+    Name="${var.env_perfix}-igw "
+  }
+} 
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myvpc-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id =aws_internet_gateway.myapp-igw.id 
+  }
+  tags = {
+    Name="${var.env_perfix}-rtb"
+  }
+}  
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id =aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id   
 }
