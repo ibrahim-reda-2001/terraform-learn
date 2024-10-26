c62ff2b67aea4935be268173c3100a4c078cd852
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2024-10-26 13:16:38 +0300

    third commit

--- main.tf
+++ main.tf
@@ -1,21 +1,30 @@
 
 provider "aws" {
-  region     = "us-east-1"
-  
+  region = "us-east-1"
+  access_key = "AKIA5CBGTGSPWMQHAX2Y"
+  secret_key = "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"
+
 }
 variable "vpc_cider_block" {
-    type = string
+  type = string
 }
 variable "subnet_cider_block" {
-  type=string
+  type = string
 }
 variable "avail_zone" {
-type = string
+  type = string
 }
 variable "env_perfix" {
   type = string
 }
 variable "myip" {
+  type = string
+}
+variable "instance_type" {
+    type = string
+  
+}
+variable "mypublic-key" {
   type = string
 }
 resource "aws_vpc" "myvpc-vpc" {
@@ -25,34 +34,34 @@ resource "aws_vpc" "myvpc-vpc" {
   }
 
 }
-  
 
-resource"aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myvpc-vpc.id
-  cidr_block = var.subnet_cider_block
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myvpc-vpc.id
+  cidr_block        = var.subnet_cider_block
   availability_zone = var.avail_zone
   tags = {
-    Name="${var.env_perfix}-subnet"
+    Name = "${var.env_perfix}-subnet"
   }
-} 
+}
 resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myvpc-vpc.id
+  vpc_id = aws_vpc.myvpc-vpc.id
   tags = {
-    Name="${var.env_perfix}-igw "
+    Name = "${var.env_perfix}-igw "
   }
-} 
+}
 /******** configure default route table *************/
 //no need for route table association
 resource "aws_default_route_table" "myapp-route-table" {
-  default_route_table_id= aws_vpc.myvpc-vpc.default_route_table_id
+  default_route_table_id = aws_vpc.myvpc-vpc.default_route_table_id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id =aws_internet_gateway.myapp-igw.id 
+    gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
-    Name="${var.env_perfix}-default-rtb"
+    Name = "${var.env_perfix}-default-rtb"
   }
-}  
+}
 
 
 
@@ -75,28 +84,28 @@ resource "aws_route_table_association" "a-rtb-subnet" {
 }*/
 /***** configure default security group ***********/
 resource "aws_default_security_group" "myapp-default-sg" {
-  
+
   vpc_id = aws_vpc.myvpc-vpc.id
   ingress {
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-            cidr_blocks = [var.myip]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.myip]
   }
   ingress {
-            from_port = 8080
-            to_port = 8080
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   tags = {
-    Name="${var.env_perfix}-defaultsg"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_perfix}-defaultsg"
   }
 }
 /*resource "aws_security_group" "myapp-sg" {
@@ -124,3 +133,37 @@ resource "aws_default_security_group" "myapp-default-sg" {
     Name="${var.env_perfix}-sg"
   }
 }*/
+/* dynamic aws iam */
+data "aws_ami" "latest-amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.6.20241010.0-kernel-6.1-x86_64"]
+  }
+}
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key-pair"
+  public_key = var.mypublic-key
+}
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true 
+  key_name = aws_key_pair.ssh-key.key_name
+  //key_name = "server-key"//this key i created from gui in aws 
+  tags = {
+    Name="${var.env_perfix}-server"
+  }
+user_data = file("entry.sh")
+}
+output "publicip" {
+  value =aws_instance.myapp-server.public_ip
+}
